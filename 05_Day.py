472324fffa8de4fd04725ac8728f7fabf15ce475
Netra KC <netrakc@SecretGhost.local>
2022-12-04 17:28:57 +0545

    Initial Commit on 5days of python

---
+++ 05_Day.py
@@ -0,0 +1,17 @@
+# Hey, netra don't remove this line.
+# Author : Netra kc. 
+# Course : 100Days of Code.
+print("Hello, I am not a good boy and\n This viewer is also a good boy or girls.")
+
+
+
+
+
+
+
+
+
+
+
+
+
